a1bec7b4ad7ad6bc1d4a8e2ced2e1c67f175a152
Aleksa-Mihajlovic <aleksa.mihajlovic.121.22@ict.edu.rs>
2025-12-20 10:17:04 +0100

    feat: added some field in IApplicationActor

--- Application/IApplicationActor.cs
+++ Application/IApplicationActor.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application
 {
-    internal interface IApplicationActor
+    public interface IApplicationActor
     {
+        public int Id { get; }
+        public string Email { get; }
+        public bool IsAdmin { get; }
+        public IReadOnlyCollection<string> ActorPermissions { get; }
     }
 }
